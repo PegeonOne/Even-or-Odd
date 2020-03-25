2a244f83401fee942116f604e262cc79e44dd256
PegeonOne <61419526+PegeonOne@users.noreply.github.com>
2020-03-25 17:17:37 +0200

    Add files via upload

---
+++ Ever-Or-Odd/Ever-Or-Odd/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ever_Or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool status = false;
+            string endApp = " ";
+            int num = 0;
+            while (!status) { 
+                Console.WriteLine("Type your number");
+                num = Convert.ToInt32(Console.ReadLine());
+                if ((~num & 1) == 1)
+                {
+                    Console.WriteLine("Even number " + num);
+                }
+                if ((~num & 1) == 0)
+                {
+                    Console.WriteLine("Odd number " + num);
+                }
+                Console.WriteLine("Type your something");
+                if (endApp == "Y")
+                {
+                    status = true;
+                }else if (endApp == "N")
+                {
+                    status = false;
+                }
+
+            }
+        }
+    }
+}
